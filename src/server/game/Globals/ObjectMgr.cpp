f59ff602d58d77dec505e1d76841623192ccde65
Dreamerydia <dreamerydia@gmail.com>
2015-05-15 17:39:55 -0400

    Object Sizing

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -2005,7 +2005,7 @@ void ObjectMgr::LoadGameobjects()
     //                                                0                1   2    3           4           5           6
     QueryResult result = WorldDatabase.Query("SELECT gameobject.guid, id, map, position_x, position_y, position_z, orientation, "
     //   7          8          9          10         11             12            13     14         15          16          17       18			19
-        "rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state, spawnMask, eventEntry, pool_entry, phaseid, phasegroup, size "
+        "rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state, spawnMask, eventEntry, pool_entry, phaseid, phasegroup, size"
         "FROM gameobject LEFT OUTER JOIN game_event_gameobject ON gameobject.guid = game_event_gameobject.guid "
         "LEFT OUTER JOIN pool_gameobject ON gameobject.guid = pool_gameobject.guid");
 
@@ -2103,8 +2103,8 @@ void ObjectMgr::LoadGameobjects()
 
         int16 gameEvent     = fields[15].GetInt8();
         uint32 PoolId       = fields[16].GetUInt32();
-		data.phaseid = fields[17].GetUInt32();
-		data.phaseGroup = fields[18].GetUInt32();
+		data.phaseid		= fields[17].GetUInt32();
+		data.phaseGroup		= fields[18].GetUInt32();
 		data.size			= fields[19].GetFloat();
 
 		if (data.size > 30.0f || data.size < 0.0f)
