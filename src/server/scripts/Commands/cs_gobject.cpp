f2667a71a552f87da7de1bbe02d04290774bed16
Dreamer Rydia <dreamerydia@gmail.com>
2015-06-16 21:19:05 -0400

    Update

--- src/server/scripts/Commands/cs_gobject.cpp
+++ src/server/scripts/Commands/cs_gobject.cpp
@@ -132,9 +132,6 @@ public:
 		// set scale
 		object->SetObjectScale(scale);
 
-		object->Relocate(object->GetPositionX(), object->GetPositionY(), object->GetPositionZ(), object->GetOrientation());
-		object->RelocateStationaryPosition(object->GetPositionX(), object->GetPositionY(), object->GetPositionZ(), object->GetOrientation());
-		object->UpdateRotationFields();
 		object->DestroyForNearbyPlayers();
 		object->UpdateObjectVisibility();
 
